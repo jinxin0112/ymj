38a7e06bf7b4b747f7c1f52421a464c238e2545e
jinxin0112 <m18508218948@163.com>
2019-10-21 11:00:34 +0800

    chore: change output path to  docs

---
+++ docs/precache-manifest.68a03ea267a01b0fde6a7a458ec28667.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21870d6338291dcc41fd0459413fb38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e02cd3367432eed3cc62",
+    "url": "/static/css/main.20dfcc30.chunk.css"
+  },
+  {
+    "revision": "ff81b6372236c66a2ae8",
+    "url": "/static/js/2.681b2d57.chunk.js"
+  },
+  {
+    "revision": "e02cd3367432eed3cc62",
+    "url": "/static/js/main.59274711.chunk.js"
+  },
+  {
+    "revision": "607800751f5508d45615",
+    "url": "/static/js/runtime-main.04e95355.js"
+  }
+]);
